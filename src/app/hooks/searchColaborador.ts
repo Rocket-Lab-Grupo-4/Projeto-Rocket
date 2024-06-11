ad501fd7066f9214b530d91da7faebf7473c95ce
RennanJSM <rennan.jsmiranda@gmail.com>
2024-06-11 15:17:48 -0300

    criação searchbar e nova branch

---
+++ src/app/hooks/searchColaborador.ts
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react';
+
+const searchColaboradores = () => {
+    const [searchColaboradores, SetSearchColaboradores] = useState([])
+}
+
+
+export default searchColaboradores;
