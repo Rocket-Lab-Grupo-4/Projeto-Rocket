ae6ad874ad733fce89828175807aa7a9e9b265da
RennanJSM <rennan.jsmiranda@gmail.com>
2024-06-12 14:26:34 -0300

    Add bloco de formulario e searchbar

--- src/app/components/searchBar/searchBar.tsx
+++ src/app/components/searchBar/searchBar.tsx
@@ -1,3 +1,37 @@
+"use client";
+
 import { useState, useEffect } from 'react';
-import styles from 'searchBar.module.scss';
-import searchColaboradores from '../../hooks/searchColaborador';
+import styles from './searchBar.module.scss';
+
+interface SearchBarProps {
+    onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+
+    const [query, setQuery] = useState('');
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+        const value = event.target.value;
+
+        setQuery(value);
+        onSearch(value);
+    }
+
+    return (
+
+        <div className={styles.searchBar}>
+
+            <input
+                type='text'
+                placeholder='Digite aqui o nome de quem deseja encontrar'
+                value={query}
+                onChange={handleInputChange}
+                className={styles.searchInput}>          
+            </input>
+
+        </div>
+    );
+};
+
+export default SearchBar;
