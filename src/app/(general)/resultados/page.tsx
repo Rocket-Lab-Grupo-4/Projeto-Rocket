2bda6775d296b959620bf2a71c3fe73b6d05efad
RennanJSM <rennan.jsmiranda@gmail.com>
2024-06-13 17:12:22 -0300

    detalhes design

--- src/app/(general)/resultados/page.tsx
+++ src/app/(general)/resultados/page.tsx
@@ -1,9 +1,14 @@
+"use client";
 import React from "react";
+import SearchBar from "@/app/components/searchBar/searchBar";
 
 export default function Resultados() {
   return (
     <>
       <p>Resultados</p>
+      <SearchBar onSearch={function (query: string): void {
+        throw new Error("Function not implemented.");
+      } } />
     </>
   );
 }
