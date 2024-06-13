3be5d2e82c5314cb883e3ef471ce97afbf8f6647
RennanJSM <rennan.jsmiranda@gmail.com>
2024-06-13 16:37:31 -0300

    alterações e api provisória

--- src/app/(general)/avaliacoes/page.tsx
+++ src/app/(general)/avaliacoes/page.tsx
@@ -1,9 +1,13 @@
+"use client;"
+
 import React from "react";
+import QuestionsPage from "@/app/components/blocoFormulario/QuestionsPage";
 
 export default function Avaliacoes() {
   return (
     <>
       <p>Avaliacoes</p>
+      <QuestionsPage />
     </>
   );
 }
