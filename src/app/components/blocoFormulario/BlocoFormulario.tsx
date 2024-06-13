9f946c97d268a845c79d3fc14999a62216d13973
RennanJSM <rennan.jsmiranda@gmail.com>
2024-06-13 16:37:31 -0300

    alterações e api provisória

--- src/app/components/blocoFormulario/BlocoFormulario.tsx
+++ src/app/components/blocoFormulario/BlocoFormulario.tsx
@@ -1,67 +1,52 @@
 "use client";
 
 import { useState } from "react";
-import styles from './BlocoFormulario.module.scss'
+import styles from './BlocoFormulario.module.scss';
 import { BlocoFormularioProps } from "@/app/interfaces/Formulario";
 
-const BlocoFormulario: React.FC<BlocoFormularioProps> = ({title, description, onResponseChange}) => {
+const BlocoFormulario: React.FC<BlocoFormularioProps> = ({ title, description, onResponseChange }) => {
+  const [response, setResponse] = useState<number | null>(null);
+  const [justification, setJustification] = useState('');
 
-    const [response, setResponse] = useState<number | null>(null);
-    const [justification, setJustification] = useState('');
+  const handleResponseChange = (value: number) => {
+    setResponse(value);
+    onResponseChange(value, justification);
+  };
 
+  const handleJustificationChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = event.target.value;
+    setJustification(value);
 
-    const handleResponseChange = (value: number) => {
-
-        setResponse(value);
-        onResponseChange(value, justification);
+    if (response !== null) {
+      onResponseChange(response, value);
     }
+  };
 
-    const handleJustificationChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-
-        const value = event.target.value;
-        setJustification(value);
-
-        if (response !== null) {
-            onResponseChange(response, value)
-        }
-    }
-
-    return (
-
-        <div className={styles.blocoFormulario}>
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <div className={styles.responseOptions}>
-                {[1, 2, 3, 4, 5].map((value) => (
-                    <label key={value}>
-                        <input
-                        type="radio"
-                        name="response"
-                        value={value}
-                        checked={response === value}
-                        onChange={() => handleResponseChange} />
-                        
-                    {value}
-                    </label>
-                ))
-                }
-            </div>
-
-            <textarea 
-                placeholder="Escreva sua justificativa aqui..."
-                value={justification}
-                onChange={handleJustificationChange}
-            
+  return (
+    <div className={styles.blocoFormulario}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <div className={styles.responseOptions}>
+        {[1, 2, 3, 4, 5].map((value) => (
+          <label key={value}>
+            <input
+              type="radio"
+              name="response"
+              value={value}
+              checked={response === value}
+              onChange={() => handleResponseChange(value)}
             />
-
-        </div>
-    )
-
-}
+            {value}
+          </label>
+        ))}
+      </div>
+      <textarea 
+        placeholder="Escreva sua justificativa aqui..."
+        value={justification}
+        onChange={handleJustificationChange}
+      />
+    </div>
+  );
+};
 
 export default BlocoFormulario;
-
-
-
-
-
