05b39f95a5d813f0fa78bd0e0e9a151c062e1955
weslley batista <wbh@cin.ufpe.br>
2024-06-20 01:43:23 -0300

    feat/avaliation: tabs

--- src/app/(general)/avaliacoes/[id]/page.tsx
+++ src/app/(general)/avaliacoes/[id]/page.tsx
@@ -1,9 +1,11 @@
 import Perfil from '@/app/components/perfil/perfil';
+import BasicTabs from '@/app/components/tabs/tabs';
 
 function historicAvaliation() {
   return ( 
   <>
     <Perfil name={'Maria'} badge={true} />
+    <BasicTabs />
   </>
  );
 }
