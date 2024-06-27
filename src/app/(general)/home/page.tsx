c2ba9d273af84f3199a5e763dfa0150977ddd394
Felipe Durant <fdf@cin.ufpe.br>
2024-06-27 18:06:41 -0300

    Header

--- src/app/(general)/home/page.tsx
+++ src/app/(general)/home/page.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import styles from './Home.module.scss';
 
 export default function Home() {
   return (
     <>
-      <p>Home</p>
+      <div className={styles.container}>
+        <div className={styles.headerRight}>
+          <div className={styles.notificationIcon}>🔔</div>
+          <div className={styles.welcomeText}>
+            <span>Bem-vinda, Maria!</span>          
+            <img src="/assets/image-perfil.png" alt="Profile" className={styles.profilePic} />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
