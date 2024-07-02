5f89b8f8d265a959b8f9cdd413c4b5b01faf16f0
Felipe Durant <fdf@cin.ufpe.br>
2024-07-02 17:46:07 -0300

    Home (Done)

--- src/app/(general)/home/page.tsx
+++ src/app/(general)/home/page.tsx
@@ -14,25 +14,20 @@ export default function Home() {
         </div>
       </div>
       <div className={styles.home}>
-        <div className={styles.bubble}>
+        <div className={styles.bubbleTextL}>
           <p>Bem-vindo ao sistema de avaliações da V-Projects!</p>
-          <br />
           <p>Queremos crescer juntos com os nossos colaboradores, por isso implementamos um sistema de ciclos avaliativos para melhor entendimento do nosso desmepenho como equipe e pessoal.</p>
-          <img src="/assets/rightBubble.png" alt="Balão Direita" className={styles.rightBubble} />
         </div>
-        <div className={styles.bubble}>
-          <label>
-            <p>O ciclo de avaliações é composto por dois tipos de avaliações, que medem critérios de desempenho e:</p>
-            <br />
-            <ol>
-              <li>Auto avaliação</li>
-              <li>Avaliação 360</li>
-            </ol>
-            <br />
-            <p>O processo dura em torno de 40 mins, então reserve um tempo e escolha um local tranquilo para iniciar o processo!</p>
-          </label>
-          <img src="/assets/leftBubble.png" alt="Balão Esquerda" className={styles.leftBubble} />
+        <div className={styles.bubbleTextR}>
+          <p>O ciclo de avaliações é composto por dois tipos de avaliações, que medem critérios de desempenho e:</p>
+          <ol>
+            <li>Auto avaliação</li>
+            <li>Avaliação 360</li>
+          </ol>
+          <p>O processo dura em torno de 40 mins, então reserve um tempo e escolha um local tranquilo para iniciar o processo!</p>
         </div>
+        <img src="/assets/leftBubble.png" alt="Balão Esquerda" className={styles.leftBubble} />
+        <img src="/assets/rightBubble.png" alt="Balão Direita" className={styles.rightBubble} />
         <button className={styles.beginButton}>Começar Avaliações</button>
         <img src="/assets/welcome-image.png" alt="Welcome" className={styles.homeWomen} />
       </div >
